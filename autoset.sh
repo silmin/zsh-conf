7559f19689ca3b762b3c63c81cb980f0d1e314da
silmin <silvestmint@gmail.com>
2019-08-16 08:46:26 +0900

    add: is darwin

--- autoset.sh
+++ autoset.sh
@@ -4,8 +4,13 @@ type zsh > /dev/null
 
 if [ $? -eq 0 ] ; then
     # zsh is installed
-    pos=`which zsh`
-    chsh -s $pos
+    #pos=`which zsh`
+    #chsh -s $pos
+
+    kernel=`uname`
+    if [ $kernel = "Darwin" ] ; then
+        # replace sed (BSD -> GNU)
+    fi
     exit 0
 else
     # zsh is not installed
