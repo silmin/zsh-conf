e5dae8668404633d691d824abed5a5a0a9b3a165
silmin <silvestmint@gmail.com>
2019-10-05 03:42:45 +0900

    delete: OSの分岐を削除

--- autoset.sh
+++ autoset.sh
@@ -16,47 +16,11 @@ if [ $? -eq 0 ] ; then
         ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
     done
 
-    kernel=`uname`
-    if [ $kernel = "Darwin" ] ; then
-        # replace sed (BSD -> GNU)
-        type brew > /dev/null
-        if [ $? -eq 0 ] ; then
-            # brew is installed
-            brew install gawk
-            brew install gnu-sed
-            
-            echo "path=(
-                /usr/local/opt/gnu-sed/libexec/gnubin(N-/)
-                /usr/local/opt/gawk/libexec/gnubin(N-/)
-                ${path}
-            )
-            manpath=(
-                /usr/local/opt/gnu-sed/libexec/gnuman(N-/)
-                /usr/local/opt/gawk/libexec/gnuman(N-/)
-                ${manpath}
-            )" >> ~/.zshrc
-
-            source ~/.zshrc
-
-            # replace theme str 'sorin' to 'pure'
-            lnum=`cat ~/.zpreztorc | grep -n theme | grep zstyle | awk -F':' '{print $1}'`
-            gsed -e "${lnum}s/sorin/pure/g" ~/.zpreztorc >| ~/.zpreztorc.new
-            \cp ~/.zpreztorc.new .zprezto/runcoms/zpreztorc     # disable aliace
-            \rm ~/.zpreztorc.new                                # disable aliace
-
-        else
-            # brew is not installed
-            echo "brew is not installed."
-            echo "please install brew."
-            exit 1
-        fi
-    else
-        # replace theme str 'sorin' to 'pure'
-        lnum=`cat ~/.zpreztorc | grep -n theme | grep zstyle | awk -F':' '{print $1}'`
-        sed -e "${lnum}s/sorin/pure/g" ~/.zpreztorc >| ~/.zpreztorc.new
-        \cp ~/.zpreztorc.new .zprezto/runcoms/zpreztorc     # disable aliace
-        \rm ~/.zpreztorc.new                                # disable aliace
-    fi
+    # replace theme str 'sorin' to 'pure'
+    lnum=`cat ~/.zpreztorc | grep -n theme | grep zstyle | awk -F':' '{print $1}'`
+    sed -e "${lnum}s/sorin/pure/g" ~/.zpreztorc >| ~/.zpreztorc.new
+    \cp ~/.zpreztorc.new .zprezto/runcoms/zpreztorc     # disable aliace
+    \rm ~/.zpreztorc.new                                # disable aliace
 
     exit 0
 
